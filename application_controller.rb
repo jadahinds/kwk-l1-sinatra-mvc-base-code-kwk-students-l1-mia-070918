57dbc4d2cd9e535ecfead7bb41cded66beffa674
jadahinds <jhinds1229@pinescharter.net>
2018-07-17 17:20:27 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,40 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
-  get '/' do
-    erb :index
+  get "/" do
+    return erb :index
+  end
+  
+  post "/" do
+    @name  = params[:user]
+    @fortune = get_fortune
+    return erb :results
   end
 
+
+
+
+
+
+
+
+
+
 end
+
+
+
+
+ #get "/video" do
+   #"This is page 2"
+ # end
+  
+  
+  
+  
+  
+  #get "/folder" do
+   # return erb :index
+  #end
